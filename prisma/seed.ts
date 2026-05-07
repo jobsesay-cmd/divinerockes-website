102751578d1242bf19dd9667cf62779ef5b78414
Junisa Sesay <jobsesay@gmail.com>
2026-05-07 01:20:53 +0000

    Revert changes to RoleType

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Role, ProjectCategoryType } from '@prisma/client';
+import { PrismaClient, RoleType, ProjectCategoryType } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
